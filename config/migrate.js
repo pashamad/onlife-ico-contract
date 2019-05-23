9244a46fafe5d665463624e19548ab3f27037fbf
PM:/ <pashamad@gmail.com>
2019-05-23 20:41:05 +0300

    Updated testnet accounts

--- config/migrate.js
+++ config/migrate.js
@@ -1,9 +1,9 @@
 module.exports = {
   ropsten: {
     accounts: {
-      migrate: '0x50c0e6177371631a2d03b78298a93Bc5bcB7446f',
-      admin: '0x52250807bE77a54672e935a60156BABdA83a3839',
-      fundsWallet: '0x7b9cb64a5a93e9983201be8e91b5276105306d58'
+      migrateAccount: '0x52250807be77a54672e935a60156babda83a3839',
+      salesOwner: '0x86ef1acc983a3b9bd76de1335747e4ee47aa97f1',
+      fundsWallet: '0x5b96d4ca405cbba1c237b249fc646783ad35600e'
     }
   }
 };
